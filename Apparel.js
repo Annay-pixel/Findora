a905c01d187e757996a6b68111c54534c2047f62
Vaibhav Mittal <vaibhav7mittal18@gmail.com>
2025-06-24 17:48:00 +0530

    Add full website with folders

---
+++ Apparel.js
@@ -0,0 +1,26 @@
+function toggleDiv2() {
+  document.getElementById("Apparel-all").style.display = "none";
+  document.getElementById("Apparel-men").style.display = "block";
+  document.getElementById("Apparel-women").style.display = "none";
+  document.getElementById("Apparel-kids").style.display = "none";
+}
+
+function toggleDiv3() {
+  document.getElementById("Apparel-all").style.display = "none";
+  document.getElementById("Apparel-women").style.display = "block";
+  document.getElementById("Apparel-kids").style.display = "none";
+  document.getElementById("Apparel-men").style.display = "none";
+}
+function toggleDiv1() {
+  document.getElementById("Apparel-all").style.display = "block";
+  document.getElementById("Apparel-women").style.display = "none";
+  document.getElementById("Apparel-men").style.display = "none";
+  document.getElementById("Apparel-kids").style.display = "none";
+}
+function toggleDiv4() {
+  document.getElementById("Apparel-all").style.display = "none";
+  document.getElementById("Apparel-kids").style.display = "block";
+  document.getElementById("Apparel-women").style.display = "none";
+  document.getElementById("Apparel-men").style.display = "none";
+
+}
---
+++ Jewellery.js
@@ -0,0 +1,8 @@
+function toggleDiv1() {
+    document.getElementById("Jewellery-women").style.display = "block";
+    document.getElementById("Jewellery-men").style.display = "none";
+}
+function toggleDiv2() {
+    document.getElementById("Jewellery-men").style.display = "block";
+    document.getElementById("Jewellery-women").style.display = "none";
+}
